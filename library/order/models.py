08ab63726b262a1ec349ee18bef82262c996a0bd
vooply <pv.yaschuk@gmail.com>
2021-03-26 15:30:12 +0200

    fix str

--- library/order/models.py
+++ library/order/models.py
@@ -18,14 +18,8 @@ class Order(models.Model):
         Magic method is redefined to show all information about Book.
         :return: book id, book name, book description, book count, book authors
         """
-        # print(Order.objects.get(id=101))
-        # return 1
-        # id = self.id
-        # return str(Order.objects.get(id=id))
-        if self.end_at is not None:
-            end_at = f"'{self.end_at}'"
-        else:
-            end_at = None
+        end_at = f"'{self.end_at}'" if self.end_at is not None else None
+
         return f"'id': {self.id}, 'user': {self.user.__class__.__name__}(id={self.user.id})," \
                f" 'book': {self.book.__class__.__name__}(id={self.book.id}), " \
                f"'created_at': '{self.created_at}', 'end_at': {end_at}, " \
